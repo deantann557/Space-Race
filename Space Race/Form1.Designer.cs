7a9c9f1f01722d8225df18cfff1730eaf98b8166
deantann557 <deantann557@SDS-L-99QLG63>
2024-01-26 11:02:26 -0500

    Add project files.

---
+++ Space Race/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Space_Race
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.P1PointLabel = new System.Windows.Forms.Label();
+            this.P2PointLabel = new System.Windows.Forms.Label();
+            this.VictoryLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // P1PointLabel
+            // 
+            this.P1PointLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.P1PointLabel.Location = new System.Drawing.Point(212, 32);
+            this.P1PointLabel.Name = "P1PointLabel";
+            this.P1PointLabel.Size = new System.Drawing.Size(100, 23);
+            this.P1PointLabel.TabIndex = 0;
+            this.P1PointLabel.Text = "P1PointLabel";
+            // 
+            // P2PointLabel
+            // 
+            this.P2PointLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.P2PointLabel.Location = new System.Drawing.Point(509, 32);
+            this.P2PointLabel.Name = "P2PointLabel";
+            this.P2PointLabel.Size = new System.Drawing.Size(87, 16);
+            this.P2PointLabel.TabIndex = 1;
+            this.P2PointLabel.Text = "P2PointLabel";
+            // 
+            // VictoryLabel
+            // 
+            this.VictoryLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.VictoryLabel.Location = new System.Drawing.Point(328, 32);
+            this.VictoryLabel.Name = "VictoryLabel";
+            this.VictoryLabel.Size = new System.Drawing.Size(139, 23);
+            this.VictoryLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.VictoryLabel);
+            this.Controls.Add(this.P2PointLabel);
+            this.Controls.Add(this.P1PointLabel);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label P1PointLabel;
+        private System.Windows.Forms.Label P2PointLabel;
+        private System.Windows.Forms.Label VictoryLabel;
+    }
+}
+
---
+++ Space Race/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Space_Race
+{
+    public partial class Form1 : Form
+    {
+        Rectangle player1 = new Rectangle(125, 170, 30, 30);
+         Rectangle player2 = new Rectangle(450, 170, 30, 30);
+
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush beigeBrush = new SolidBrush(Color.Beige);
+
+        int player1Speed = 6;
+        int player2Speed = 6;
+
+        int player1Score = 0;
+        int player2Score = 0;
+
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+
+        Random randGen = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            P1PointLabel.Text = $"Player 1: {player1Score}";
+            P2PointLabel.Text = $"Player 2: {player2Score}";
+           
+
+            e.Graphics.FillRectangle(blueBrush, player1);
+            e.Graphics.FillRectangle(beigeBrush, player2);
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            if (wDown == true && player1.Y > 2)
+            {
+                player1.Y -= player1Speed;
+            }
+            if (sDown == true && player1.Y < 334)
+            {
+                player1.Y += player1Speed;
+            }
+            if (player1.Y < 3)
+            {
+                player1.Y = randGen.Next(333, 334);
+                player1Score++;
+            }
+
+            if (upArrowDown == true && player2.Y > 2)
+            {
+                player2.Y -= player2Speed;
+            }
+            if (downArrowDown == true && player2.Y < 334)
+            {
+                player2.Y += player2Speed;
+            }
+            if (player2.Y < 3)
+            {
+                player2.Y = randGen.Next(333, 334);
+                player2Score++;
+            }
+
+            if (player1Score == 5)
+            {
+                gameTimer.Enabled = false;
+                VictoryLabel.Visible = true;
+                VictoryLabel.Text = "Player 1 Wins!!";  
+            }
+            else if (player2Score == 5)
+            {
+                gameTimer.Enabled = false;
+                VictoryLabel.Visible = true;
+                VictoryLabel.Text = "Player 2 Wins!!";
+            }
+            Refresh();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+            }
+
+
+            {
+
+                {
+
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+        }
+
+       
+    }
+}
+
